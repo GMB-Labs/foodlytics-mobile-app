0a7e2a828bc3f438cd63413a9fd35d8981845e34
Belen Ramos <129527512+BelenramosR@users.noreply.github.com>
2025-10-24 20:06:42 -0500

    fix dependencies

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,17 @@
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
-
-
-SplashScreen.preventAutoHideAsync();
+import './global.css';
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) SplashScreen.hideAsync();
-  }, [loaded]);
-
-  if (!loaded) return null;
-
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
